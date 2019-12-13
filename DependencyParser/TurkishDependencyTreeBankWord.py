757984c9862491ed72625d2d3c1ea0ae66a23294
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-12-13 23:27:53 +0300

    Updated DependencyParser repository translation.

--- DependencyParser/TurkishDependencyTreeBankWord.py
+++ DependencyParser/TurkishDependencyTreeBankWord.py
@@ -8,3 +8,56 @@ class TurkishDependencyTreeBankWord(Word):
     __parse: MorphologicalParse
     __originalParses: list
     __relation: TurkishDependencyRelation
+
+    """
+    Accessor for the parse attribute
+    
+    RETURNS
+    -------
+    MorphologicalPArse
+        Parse attribute
+    """
+    def getParse(self) -> MorphologicalParse:
+        return self.__parse
+
+    """
+    Accessor for a specific parse.
+    
+    PARAMETERS
+    ----------
+    index : int
+        Index of the word.
+        
+    RETURNS
+    -------
+    MorphologicalParse
+        Parse of the index'th word
+    """
+    def getOriginalParse(self, index: int) -> MorphologicalParse:
+        if index < len(self.__originalParses):
+            return self.__originalParses[index]
+        else:
+            return None
+
+    """
+    Number of words in this item.
+
+    RETURNS
+    -------
+    int
+        Number of words in this item.
+    """
+
+    def size(self) -> int:
+        return len(self.__originalParses)
+
+    """
+    Accessor for the relation attribute.
+
+    RETURNS
+    -------
+    TurkishDependencyRelation
+        relation attribute.
+    """
+    def getRelation(self) -> TurkishDependencyRelation:
+        return self.__relation
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-DependencyParser',
+    version='1.0.0',
+    packages=['DependencyParser'],
+    url='',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Turkish Dependency Parser'
+)
