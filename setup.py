d8fbd52cfa622f9a13c14e61d5a04b1ad3e654cf
olcaytaner <olcaytaner@isikun.edu.tr>
2019-12-30 16:25:40 +0300

    Removed unnecessary dependence of NlpToolkit-Corpus.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-DependencyParser',
-    version='1.0.2',
+    version='1.0.3',
     packages=['DependencyParser'],
     url='https://github.com/olcaytaner/TurkishDependencyParser-Py',
     license='',
     author='olcaytaner',
     author_email='olcaytaner@isikun.edu.tr',
     description='Turkish Dependency Parser',
-    install_requires=['NlpToolkit-MorphologicalAnalysis', 'NlpToolkit-Corpus']
+    install_requires=['NlpToolkit-MorphologicalAnalysis']
 )
