81412b87e4a329e9843bb9753ab2c97fb00ea66d
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2020-11-06 10:24:11 +0300

    Removed universal dependency relation tags Nsubjpass, Csubjpass and Neg.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NlpToolkit-DependencyParser',
-    version='1.0.10',
+    version='1.0.11',
     packages=['DependencyParser'],
     url='https://github.com/olcaytaner/TurkishDependencyParser-Py',
     license='',
