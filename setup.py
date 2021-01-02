a31921295523ac40271d7250d4513d7022f46361
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-01-02 13:18:16 +0300

    Updated e-mail and url.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='NlpToolkit-DependencyParser',
     version='1.0.13',
     packages=['DependencyParser', 'DependencyParser.Turkish', 'DependencyParser.Universal', 'DependencyParser.Stanford'],
-    url='https://github.com/olcaytaner/TurkishDependencyParser-Py',
+    url='https://github.com/StarlangSoftware/TurkishDependencyParser-Py',
     license='',
     author='olcaytaner',
-    author_email='olcaytaner@isikun.edu.tr',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
     description='Turkish Dependency Parser',
     install_requires=['NlpToolkit-MorphologicalAnalysis']
 )
