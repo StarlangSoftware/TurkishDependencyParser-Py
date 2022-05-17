283b2ee027619a16c6fddd740ac95387f65335ef
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-05-17 23:18:54 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='NlpToolkit-DependencyParser',
-    version='1.0.20',
+    version='1.0.21',
     packages=['DependencyParser', 'DependencyParser.Turkish', 'DependencyParser.Universal', 'DependencyParser.Stanford'],
     url='https://github.com/StarlangSoftware/TurkishDependencyParser-Py',
     license='',
